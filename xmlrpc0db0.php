e594bea2afd052453f35b9e9fbcba1c67893e205
jizhang88 <jizhang88@hotmail.com>
2023-11-19 22:28:30 -0500

    rre
    rere

--- xmlrpc0db0.php
+++ xmlrpc0db0.php
@@ -2,13 +2,13 @@
 	<service>
 		<engineName>WordPress</engineName>
 		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>https://dashunfur.com</homePageLink>
+		<homePageLink>http://dashunfur.com</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="https://dashunfur.com/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://dashunfur.com/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://dashunfur.com/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="https://dashunfur.com/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="https://dashunfur.com/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="http://dashunfur.com/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="http://dashunfur.com/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://dashunfur.com/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="http://dashunfur.com/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="http://dashunfur.com/wp-json/" />
 			</apis>
 	</service>
 </rsd>
